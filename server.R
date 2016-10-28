020d2e80bfa2a4959e0e302c786889408464f254
dtemple <dtemple@gmail.com>
2016-10-27 22:28:57 -0400

    initial commit

--- server.R
+++ server.R
@@ -1,26 +1,49 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+
+Bdata<-read.csv("all-data.csv")
+predictor<-as.data.frame(Bdata)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'skyblue', border = 'white')
-    
+  
+  datasetInput <- reactive({
+    switch(input$var,
+           "All Historic Data" = predictor[,2],
+           "Last 7 Days" = predictor[,3],
+           "Last Month" = predictor[,4],
+           "Last 3 Months" = predictor[,5])
   })
   
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  output$distPlot <- renderPlot({
+    color<-switch(input$var,
+                  "All Historic Data" = "darkgreen",
+                  "Last 7 Days" = "skyblue",
+                  "Last Month" = "darkviolet",
+                  "Last 3 Months" = "black")
+    
+    dataset <- datasetInput()
+    
+#    display<-switch(input$display,
+#                    "Guests by Usage Likelihood"= predictor[, 2],
+#                    "Guests by Predicted Value"= predictor[,2])
+    
+#    x <- predictor[, 2]  # Prediction Data
+#BINS    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    dataset<-subset(dataset, dataset > input$range[1] & dataset < input$range[2])
+    
+    # draw the histogram with the specified number of bins
+#BINS    hist(x, breaks = bins, col = 'darkgray', border = 'white', main = "Distribution by Likelihood to use Hello Scout", xlab = "Likelihood to Use Hello Scout (%)", ylab = "Number of Guests")
+    hist(dataset, col = color, border = 'white', main = "Distribution by Likelihood to use Hello Scout", xlab = "Likelihood to Use Hello Scout (%)", ylab = "Number of Guests")
+  })
+  
+  output$view <- renderText({
+    paste("You input", paste(input$range, collapse=''))
+  })
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,54 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("San Francisco Arrivals Next 5 Days"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 5,
-                   max = 50,
-                   value = 30)
-    ),
+      helpText("Filters for all arriving guests"),
+      
+#      sliderInput("bins",
+#                  "Number of bins:",
+#                  min = 1,
+#                  max = 50,
+#                  value = 30)
+#    ),
     
+#    radioButtons("display",
+#             label = "Display",
+#             choices = list("Guests by Usage Likelihood", "Guests by Predicted Value"),
+#             selected = "Guests by Usage Likelihood"),
+
+    sliderInput("range", 
+                label = "Likelihood to Use Hello Scout (%):",
+                min = 0, max = 100, value = c(20, 100)),
+ 
+#sliderInput("bins",
+#            "Number of bins:",
+#            min = 1,
+#            max = 50,
+#            value = 30),
+ 
+#  sliderInput("value-range", 
+#              label = "Gross Booking Predictor ($$):",
+#              min = 1, max = 75, value = c(1, 75)),
+
+    selectInput("var", 
+                label = "Compare arrivals with",
+                choices = list("All Historic Data", "Last 7 Days",
+                               "Last Month", "Last 3 Months"),
+                selected = "All Historic Data")
+),
+
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      #tableOutput("view"),
+      plotOutput("distPlot"),
+      verbatimTextOutput("summary")
     )
   )
 ))
